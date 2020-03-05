391c12ef4124e35b949ed31e83aa8ed417ba2a8f
lauremtm69 <alenmutum21@gmail.com>
2020-03-05 21:31:05 +0530

    finish and logout or sign out

--- app/src/main/java/com/alenmutum21/ac_twitterclone/SignUpLoginActivity.java
+++ app/src/main/java/com/alenmutum21/ac_twitterclone/SignUpLoginActivity.java
@@ -1,5 +1,6 @@
 package com.alenmutum21.ac_twitterclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,7 +45,10 @@ public class SignUpLoginActivity extends AppCompatActivity {
                     @Override
                     public void done(ParseException e) {
                         if (e == null){
-                            FancyToast.makeText(SignUpLoginActivity.this,appUser.get("username")+" succesfully signed up",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                            FancyToast.makeText(SignUpLoginActivity.this,appUser.getUsername()+" succesfully signed up",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                            Intent sendUserToWelcomeActivity = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            startActivity(sendUserToWelcomeActivity);
+                            finish();
 
                         }else {
                             String error = e.getMessage();
@@ -66,6 +70,10 @@ public class SignUpLoginActivity extends AppCompatActivity {
                     public void done(ParseUser user, ParseException e) {
                         if (user != null && e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,"succesfully Logged in",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                            Intent sendUserToWelcomeActivity = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            sendUserToWelcomeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(sendUserToWelcomeActivity);
+                            finish();
 
                         }else {
                             FancyToast.makeText(SignUpLoginActivity.this,"error: "+ e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,false).show();
--- app/src/main/java/com/alenmutum21/ac_twitterclone/WelcomeActivity.java
+++ app/src/main/java/com/alenmutum21/ac_twitterclone/WelcomeActivity.java
@@ -2,13 +2,39 @@ package com.alenmutum21.ac_twitterclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        textView = findViewById(R.id.textView);
+        signOut = findViewById(R.id.signout);
+
+
+        String username = ParseUser.getCurrentUser().getUsername();
+
+        textView.setText(username);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                startActivity(new Intent(WelcomeActivity.this,SignUpLoginActivity.class));
+                FancyToast.makeText(WelcomeActivity.this,"Logout",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                finish();
+            }
+        });
     }
 }
